8b2a92771bd427d3a2c7296d6139401a489df09e
panyurou <yrpan@jinshuju.net>
2021-10-04 22:02:06 +0800

    构建方法引用

--- src/main/java/com/webflux/MethodReferenceDemo.java
+++ src/main/java/com/webflux/MethodReferenceDemo.java
@@ -1,25 +1,32 @@
 package com.webflux;
 
+import java.util.function.Function;
 import java.util.function.IntUnaryOperator;
+import java.util.function.Supplier;
 
 public class MethodReferenceDemo {
     static class Dog {
-        private int food_weight = 10;
+        private String name = "啸天犬";
 
-//      JDk会默认把当前实例传入到非静态方法，参数名为this，位置是第一个
-        public int eat(Dog this, int weight) {
-            System.out.println("小狗吃了" + weight + "斤狗粮");
-            this.food_weight -= weight;
-            return this.food_weight;
+        public Dog() {
+
+        }
+
+        @Override
+        public String toString() {
+            return this.name;
+        }
+
+        public Dog(String name) {
+            this.name = name;
         }
     }
 
     public static void main(String[] args) {
-        Dog dog = new Dog();
-//        Function<Integer, Integer> function = dog::eat;
-//        UnaryOperator<Integer> function = dog::eat;
-        IntUnaryOperator function = dog::eat;
-        int weight = function.applyAsInt(3);
-        System.out.println("还剩余" + weight + "斤狗粮");
+        Supplier<Dog> supplier = Dog::new;
+        System.out.println(supplier.get());
+
+        Function<String,Dog> function2 =  Dog::new;;
+        System.out.println(function2.apply("小杂毛"));
     }
 }
