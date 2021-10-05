40286cb968a8ffbe5f46bb9f3489bf486c2f17d1
panyurou <yrpan@jinshuju.net>
2021-10-05 19:09:07 +0800

    多次调用parallel / sequential，以最后一个为准

--- src/main/java/com/webflux/stream/ParallelStream.java
+++ src/main/java/com/webflux/stream/ParallelStream.java
@@ -4,9 +4,17 @@ import java.util.stream.IntStream;
 
 public class ParallelStream {
     public static void main(String[] args) {
-        IntStream.range(1,100).parallel().peek(ParallelStream::debugger).count();
+        IntStream.range(1, 10)
+                .parallel().peek(ParallelStream::debugger)
+                .sequential().peek(ParallelStream::debugger2)
+                .count();
     }
+
+    private static void debugger2(int i) {
+        System.err.println("debugger2: " + i);
+    }
+
     private static void debugger(int i) {
-        System.out.println("debugger"+i);
+        System.out.println("debugger1: " + i);
     }
 }
