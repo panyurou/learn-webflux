28404c658e42bc2709c528293b3ce2f577c8cc9f
panyurou <yrpan@jinshuju.net>
2021-10-06 13:02:46 +0800

    并行流

--- src/main/java/com/webflux/stream/ParallelStream.java
+++ src/main/java/com/webflux/stream/ParallelStream.java
@@ -1,20 +1,27 @@
 package com.webflux.stream;
 
+import java.util.concurrent.ForkJoinPool;
 import java.util.stream.IntStream;
 
 public class ParallelStream {
     public static void main(String[] args) {
-        IntStream.range(1, 10)
+        ForkJoinPool forkJoinPool = new ForkJoinPool(20);
+        forkJoinPool.submit(() -> IntStream.range(1, 10)
                 .parallel().peek(ParallelStream::debugger)
-                .sequential().peek(ParallelStream::debugger2)
-                .count();
-    }
+                .count());
+        forkJoinPool.shutdown();
 
-    private static void debugger2(int i) {
-        System.err.println("debugger2: " + i);
+        synchronized (forkJoinPool) {
+            try {
+                forkJoinPool.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 
     private static void debugger(int i) {
-        System.out.println("debugger1: " + i);
+        System.out.println(Thread.currentThread().getName() + "debugger1: " + i);
     }
 }
